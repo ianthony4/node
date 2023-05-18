1acc9e5f0d0d760d7a51b09839aa9dc1d4e98949
klismannSis <achaisa@unsa.edu.pe>
2023-05-18 12:15:17 -0500

    Modificando el JS

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const http = require('http');
-const server = http.createServer((request, response) => {
-	console.log(request.url);
-	response.end("Hola mundo");
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+
+	console.log("Escuchando en: http://localhost:3000");
 });
-server .listen(3000);
-console.log("Escuchando en: http://localhost:3000");
+
+app.get('/',(request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
